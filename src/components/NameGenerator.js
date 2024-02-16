b7d0cdd9bd23b29f735a81d2144043956e173644
unknown <zachmoore223@gmail.com>
2024-02-16 13:27:27 -0500

    more suffs and pres

--- src/components/NameGenerator.js
+++ src/components/NameGenerator.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 
 const NameGenerator = () => {
-  const prefixes = ['Aero', 'Anti', 'Ash', 'Ast', 'Aster', 'Astro', 'Astra', 'Cos', 'Cosmo', 'Dextro', 'Dextra', 'Galactic', 'Gala', 'Glob', 'Globu', 'Globula', 'Hydra', 'Hydro', 
-  'Interstella', 'Interstellar', 'Luna', 'Lun', 'Lux', 'Luxa', 'Luxo', 'Lumi', 'Magna', 'Mecha', 'Meta', 'Nebula', 'Neutrino', 'Noct', 'Nocto', 'Nova', 'Omni', 'Sol', 'Sphere', 
-  'Stella', 'Sub', 'Super', 'Terra', 'Turbo', 
-  'Void', 'Xeno'];
+  const prefixes = [
+  'Aero', 'Ash', 'Ast', 'Aster', 'Astro', 'Astra', 'Cometo', 'Comet', 'Cos', 'Cosmo', 'Cryo', 'Crypt', 'Crypto', 'Dextro', 'Dextra', 
+  'Dyno', 'Dynamo', 'Eclip', 'Eclipto', 'Eclipt', 'Exo', 'Exoplaneto', 'Equa', 
+  'Galactic', 'Gala', 'Galacto', 'Galac','Galaga', 'Galag', 'Glob', 'Geo', 'Globu', 'Globula', 'Helio', 'Hel', 'Heli', 'Heliaco', 'Hydra', 'Hydro', 
+  'Hyper', 'Hypo', 'Hypergiant', 'Interstella', 'Interstellar', 'Inter', 'Intra', 
+  'Luna', 'Lun', 'Lux', 'Luxa', 'Luxo', 'Lumi', 'Macro', 'Mac', 'Magna', 'Magneto', 'Magnet',
+  'Mecha', 'Meso', 'Meteor', 'Meteora', 'Meteoro', 'Meta', 'Micro', 
+  'Mic', 'Neo', 'Nebula', 'Neutrino', 'Noct', 'Nocto', 'Nova', 'Novo', 'Novum', 
+  'Omni', 'Paleo', 'Pale', 'Poly', 'Pol', 'Planet', 'Plasma', 'Plasm', 'Proto', 'Quasar', 'Radio', 'Radi', 'Supernov', 'Supernova', 'Solar', 
+  'Sidero', 'Stellaro', 'Sol', 'Sphere', 'Stella', 'Sub', 'Sublo', 'Super', 'Tele', 'Terr', 'Terra', 'Thermo', 'Turbo', 'Uranus', 'Ven', 
+  'Volcan', 'Volcano', 'Vulcan', 'Velo', 'Vel', 
+  'Vena', 'Veno', 'Void', 'Xeno', 'Zod', 'Zoda', 'Zedo', 'Zeno'];
 
-  const suffixes = ['acid', 'acrid', 'adon', 'agogue', 'aplex', 'arch', 'abiotic', 'abionic', 'abot', 'achrome', 'adon', 'atron', 'asaur', 'asaurus', 'atome', 'biotic', 'bionic', 'bot', 'botic', 'borg', 'chrome', 
-  'cid', 'crid', 'crpyt', 'dome', 'drome', 'don', 'droid', 'eidos', 'ellite', 'ergy', 
-  'genesis', 'gram', 'hedra', 'hedron', 'ic', 'idon', 'ionic', 'micon', 'morph', 'naut', 'noid', 'obot', 'ochrome', 'odon', 'omic', 'omicon', 'onaut', 'onic', 'oplex', 'oid', 'otron', 
-  'phage', 'plex', 'ploid', 'pod', 'roid', 'saur', 'saurus', 'tome', 
-  'toid', 'tron', 'tronic', 'zoid'];
+  const suffixes = [
+  'acid', 'acrid', 'adon', 'agogue', 'aplex', 'arch', 'abiotic', 'abionic', 'abot', 'achrome', 'adon', 'atron', 'asaur', 'asaurus', 'atome', 
+  'atoid', 'atron', 'atronic', 'azoid', 'aborg', 'abit', 'abyte', 'ablob', 'abrok', 'ablock',  'abric', 'ablic', 'arium', 'anom', 'amon', 'aton',
+  'aburn', 'burn',
+  'biotic', 'bit', 'bionic', 'blob', 'block', 'blic', 'bot', 'botic', 'borg', 'brok', 'bric', 'byte', 'chrome',   'cid', 'crid', 'crpyt', 
+  'dome', 'drome', 'don', 'droid', 'eidos', 'ellite', 'ergy', 'genesis', 'gram', 'hedra', 'hedron', 'ic', 'idon', 'ionic', 'ium', 
+  'micon', 'morph', 'mon', 'naut', 'noid', 'nom', 'obot', 'ochrome', 'odon', 'omic', 'omicon', 'onaut', 'onic', 'oplex', 'oid', 'otron', 'oton',
+  'ocid', 'ocrid', 'odon', 'oplex', 'obiotic', 'obionic', 'osaur', 'osaurus', 'otome', 'obyte', 'obrok', 'orium', 'obrun',
+  'otoid', 'otron', 'otronic', 'ozoid', 'oborg', 'obit', 'oblob', 'oblock', 'obric', 'oblic', 'onom', 'omon',
+  'phage', 'plex', 'ploid', 'pod', 'rium', 'roid', 'saur', 'saurus', 'tome', 'toid', 'tron', 'tronic', 'ton', 'um', 'zoid'
+];
 
   const [generatedName, setGeneratedName] = useState('');
 
